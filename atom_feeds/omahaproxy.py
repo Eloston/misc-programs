d1d438104968c209045fa587b7709751e151654c
Eloston <eloston@programmer.net>
2018-09-04 05:10:47 +0000

    Update omahaproxy feed to read content from stdin

--- atom_feeds/omahaproxy.py
+++ atom_feeds/omahaproxy.py
@@ -1,44 +1,53 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-# Spits out Omahaproxy stable channel info to stdout in Atom format
-# Atom code is from https://github.com/rpcope1/pyatom
+"""
+Spits out Omahaproxy stable channel info to stdout in Atom format
+It reads the content from a URL like
+https://omahaproxy.appspot.com/history.json?channel=stable from standard input
+Atom code is from https://github.com/rpcope1/pyatom
+"""
 
 import sys
 import datetime
-import urllib.request
 import json
 
 import pyatom
 
-if __name__ == "__main__":
-    SOURCE_URL = "https://omahaproxy.appspot.com/history.json?channel=stable"
-    OS_ID_CONVERSION = {
-        "ios": "iOS",
-        "cros": "Chrome OS",
-        "win": "Windows",
-        "win64": "Windows 64-bit",
-        "linux": "Linux",
-        "mac": "Mac OSX",
-        "cf": "Chrome Frame",
-        "android": "Android",
-        "webview": "Android WebView"
-    }
+OS_ID_CONVERSION = {
+    "ios": "iOS",
+    "cros": "Chrome OS",
+    "win": "Windows",
+    "win64": "Windows 64-bit",
+    "linux": "Linux",
+    "mac": "Mac OSX",
+    "cf": "Chrome Frame",
+    "android": "Android",
+    "webview": "Android WebView"
+}
+
+
+def main():
+    """Entrypoint"""
 
     def init_feed():
-        return pyatom.AtomFeed(title="Chrome Stable Releases",
-                    subtitle="Stable release versions from Omahaproxy",
-                    feed_url=SOURCE_URL,
-                    url="https://omahaproxy.appspot.com/")
+        """Initialize feed"""
+        return pyatom.AtomFeed(
+            title="Chrome Releases",
+            subtitle="Chrome releases from Omahaproxy",
+            url="https://omahaproxy.appspot.com/")
 
-    def exception_hook(*args):
+    def exception_hook(exc_type, exc_value, exc_traceback):
+        """Generates atom feed for reporting exceptions"""
         feed = init_feed()
         time_of_exception = datetime.datetime.utcnow()
         import traceback
-        feed.add(title="Exception has occured!",
-                content="<p>" + "<br />".join(traceback.format_exception(*args)) + "</p>",
-                content_type="html",
-                updated=time_of_exception,
-                id=str(time_of_exception))
+        feed.add(
+            title="Exception has occured!",
+            content="<p>" + "<br />".join(
+                traceback.format_exception(exc_type, exc_value, exc_traceback)) + "</p>",
+            content_type="html",
+            updated=time_of_exception,
+            id=str(time_of_exception))
         print(feed.to_string())
         sys.exit()
 
@@ -46,24 +55,31 @@ if __name__ == "__main__":
 
     feed = init_feed()
 
-    with urllib.request.urlopen(SOURCE_URL) as response:
-        json_encoder = json.JSONEncoder(indent=4)
-        for version in json.loads(response.read().decode("UTF-8")):
-            if len(feed.entries) > 50:
-                break
-            version_date, version_time = version["timestamp"].split(" ")
-            version_year, version_month, version_day = (int(x, 10) for x in version_date.split("-"))
-            if "." in version_time:
-                version_hour, version_minute, version_second, version_microsecond = (int(x, 10) for x in version_time.replace(".", ":").split(":"))
-            else:
-                version_hour, version_minute, version_second = (int(x, 10) for x in version_time.split(":"))
-                version_microsecond = 0
-            item_updated = datetime.datetime(version_year, version_month, version_day, version_hour, version_minute, version_second, version_microsecond)
-            feed.add(title=OS_ID_CONVERSION[version["os"]] + ": " + version["version"],
-                    content="<p>" + json_encoder.encode(version) + "</p>",
-                    content_type="html",
-                    updated=datetime.datetime(version_year, version_month, version_day, version_hour, version_minute, version_second, version_microsecond),
-                    url="https://omahaproxy.appspot.com/deps.json?version=" + version["version"],
-                    id=version["os"] + "_" + version["version"])
+    json_encoder = json.JSONEncoder(indent=4)
+    for version in json.loads(sys.stdin.read()):
+        if len(feed.entries) > 50:
+            break
+        version_date, version_time = version["timestamp"].split(" ")
+        version_year, version_month, version_day = (int(x, 10) for x in version_date.split("-"))
+        if "." in version_time:
+            version_hour, version_minute, version_second, version_microsecond = (int(
+                x, 10) for x in version_time.replace(".", ":").split(":"))
+        else:
+            version_hour, version_minute, version_second = (int(x, 10)
+                                                            for x in version_time.split(":"))
+            version_microsecond = 0
+        item_updated = datetime.datetime(version_year, version_month, version_day, version_hour,
+                                         version_minute, version_second, version_microsecond)
+        feed.add(
+            title=OS_ID_CONVERSION[version["os"]] + ": " + version["version"],
+            content="<p>" + json_encoder.encode(version) + "</p>",
+            content_type="html",
+            updated=item_updated,
+            url="https://omahaproxy.appspot.com/deps.json?version=" + version["version"],
+            id=version["os"] + "_" + version["version"])
 
     print(feed.to_string())
+
+
+if __name__ == "__main__":
+    main()
