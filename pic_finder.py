50c51098277b0aef0cfff7a86d6e484538f63b77
Eloston <eloston@programmer.net>
2021-06-07 01:35:16 +0000

    pic_finder.py: Remove caching of keypoint sets, add resize argument

--- pic_finder.py
+++ pic_finder.py
@@ -20,13 +20,9 @@ The processing pipeline and choice of algorithms are not my own.
 NTOP = 50
 # ANSI escape code to clear line
 ANSI_CL = '\033[K'
-# Descriptors are np.ndarray objects serialized via np.save
-_DESCRIPTOR_FILE_SUFFIX = '.npy'
 
 # Globals for image scanning algorithms
 
-# Length of largest dimension when downscaling images
-RESIZED_DIMENSION = 2048
 # Lowe ratio, essentially used to determine if two keypoints match
 LOWE_RATIO = 0.7
 # Requires OPENCV_ENABLE_NONFREE=ON when compiling OpenCV
@@ -45,17 +41,12 @@ flann_index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
 flann_search_params = dict(checks=50) # or pass empty dictionary
 _FLANN = cv2.FlannBasedMatcher(flann_index_params, flann_search_params)
 
-# Globals for a worker in the build command process pool
-
-# Source image root directory
-_IMG_ROOT = None
-# Descriptor database root directory
-_DB_ROOT = None
-
-# Globals for a worker in the query command process pool
+# Globals for a worker in the process pool
 
 # Loaded reference descriptor
 _REFERENCE_DES: np.ndarray = None
+# Maximum length of the larger dimension when downscaling images
+_MAX_LENGTH: int = None
 
 # Base code for image searching
 
@@ -65,24 +56,24 @@ class _InvalidComputation(Exception):
     pass
 
 
-def downscale_img(img):
+def downscale_img(img, max_length):
     height, width = img.shape[:2]
-    if height < RESIZED_DIMENSION and width < RESIZED_DIMENSION:
+    if height < max_length and width < max_length:
         # Image is already smaller than we need
         return img
     if height > width:
-        new_dim = (int(width / float(height) * RESIZED_DIMENSION), RESIZED_DIMENSION)
+        new_dim = (int(width / float(height) * max_length), max_length)
     else:
-        new_dim = (RESIZED_DIMENSION, int(height / float(width) * RESIZED_DIMENSION))
+        new_dim = (max_length, int(height / float(width) * max_length))
     return cv2.resize(img, new_dim, interpolation=cv2.INTER_AREA)
 
 
-def compute_descriptor(img_path):
+def compute_descriptor(img_path, max_length):
     img = cv2.imread(str(img_path), 0)
     if img is None:
         raise _InvalidComputation('Not a valid image')
 
-    img = downscale_img(img)
+    img = downscale_img(img, max_length)
 
     #kp, des = _ORB.detectAndCompute(img, None)
     kp, des = _SIFT.detectAndCompute(img, None)
@@ -95,14 +86,6 @@ def compute_descriptor(img_path):
         raise _InvalidComputation('descriptor is None')
 
     return des
-
-
-def load_descriptor(des_path):
-    return np.load(des_path, allow_pickle=False)
-
-
-def save_descriptor(img_des, des_path):
-    np.save(des_path, img_des, allow_pickle=False)
 
 
 def get_good_matches(des1, des2):
@@ -130,15 +113,16 @@ def get_good_matches(des1, des2):
 # Process pool worker functions
 
 
-def init_build_worker(source_imgs, db_path):
-    global _IMG_ROOT, _DB_ROOT
-    _IMG_ROOT = source_imgs
-    _DB_ROOT = db_path
+def init_worker(img_path, max_length):
+    global _REFERENCE_DES, _MAX_LENGTH
+    assert _REFERENCE_DES is None
+    assert _MAX_LENGTH is None
+    _REFERENCE_DES = compute_descriptor(img_path, max_length)
+    _MAX_LENGTH = max_length
 
 
-def db_add(img_path):
-    assert _IMG_ROOT is not None
-    assert _DB_ROOT is not None
+def compute_ngood(img_path):
+    assert _REFERENCE_DES is not None
     if not img_path.is_file():
         print('\nSkipping non-file', img_path)
         return
@@ -149,80 +133,22 @@ def db_add(img_path):
     try:
         print(f'{ANSI_CL}Processing', img_path, end='\r')
         try:
-            img_des = compute_descriptor(img_path)
+            scan_des = compute_descriptor(img_path, _MAX_LENGTH)
         except _InvalidComputation as exc:
             print(f'{ANSI_CL}Skipping img with compute_descriptor error "{str(exc)}":', img_path)
-            return
-        # des_path does not contain .npy suffix because that will be added by np.save
-        des_path = _DB_ROOT / img_path.relative_to(_IMG_ROOT)
-        des_path.parent.mkdir(parents=True, exist_ok=True)
-        save_descriptor(img_des, des_path)
-    except BaseException as exc:
-        print(f'{ANSI_CL}Threw exception on {img_path}: {exc}')
-
-
-def init_query_worker(des_path):
-    global _REFERENCE_DES
-    assert _REFERENCE_DES is None
-    _REFERENCE_DES = load_descriptor(des_path)
-
-
-def compute_ngood(des_path):
-    assert _REFERENCE_DES is not None
-    try:
-        print(f'{ANSI_CL}Processing', des_path, end='\r')
-        scan_des = load_descriptor(des_path)
+            return -1, img_path
         try:
             ngood = get_good_matches(_REFERENCE_DES, scan_des)
         except _InvalidComputation as exc:
-            print(f'{ANSI_CL}Skipping img with get_good_matches error "{str(exc)}":', des_path)
-            return -1, des_path
+            print(f'{ANSI_CL}Skipping img with get_good_matches error "{str(exc)}":', img_path)
+            return -1, img_path
     except BaseException as exc:
-        print(f'{ANSI_CL}Threw exception on {des_path}: {exc}')
-        return -1, des_path
-    return ngood, des_path
+        print(f'{ANSI_CL}Threw exception on {img_path}: {exc}')
+        return -1, img_path
+    return ngood, img_path
 
 
 # Main process functions
-
-
-def build_cmd(args, parser_error):
-    # Sanity check arguments
-    if not args.db.is_dir():
-        parser.error('Database path is not an existing directory: ' + str(args.db))
-    if not args.source_imgs.is_dir():
-        parser_error('Source images path is not an existing directory: ' + str(args.source_imgs))
-
-    with multiprocessing.Pool(args.workers, init_build_worker, [args.source_imgs, args.db]) as pool:
-        for _ in pool.imap_unordered(db_add, args.source_imgs.rglob('*'), chunksize=args.chunksize):
-            pass
-    # Add newline because our cursor is at the beginning of a line with text
-    print()
-
-
-def prepare_cmd(args, parser_error):
-    # Sanity check arguments
-    if not args.query_img.is_file():
-        parser_error('Input image is not an existing file: ' + str(args.query_img))
-
-    reference_des = compute_descriptor(args.query_img)
-    save_descriptor(reference_des, args.query_des)
-
-
-def query_cmd(args, parser_error):
-    # Sanity check arguments
-    if not args.db.is_dir():
-        parser.error('Database path is not an existing directory: ' + str(args.db))
-    if not args.query_des.is_file():
-        parser_error('Query descriptor is not an existing file: ' + str(args.query_des))
-
-    with multiprocessing.Pool(args.workers, init_query_worker, [args.query_des]) as pool:
-        all_matches = list(
-            pool.imap_unordered(compute_ngood, args.db.rglob('*'), chunksize=args.chunksize))
-    all_matches.sort()
-    print(f'{ANSI_CL}Top {NTOP} matches:')
-    for ngood, scan_path in all_matches[-NTOP:]:
-        print(scan_path, f'({ngood})')
 
 
 def main():
@@ -237,39 +163,21 @@ def main():
                         type=int,
                         default=2,
                         help='Chunksize for multiprocessing map operation. Default: %(default)s')
-    subparsers = parser.add_subparsers()
-    parser_build = subparsers.add_parser(
-        'build', help='Build the descriptor database, updating as necessary')
-    parser_build.add_argument('--db',
-                              required=True,
+    parser.add_argument('--resize', type=int, default=640, help='Before computing descriptors, specifies the maximum length of the larger dimension when downscaling images. Default: %(default)s')
+    parser.add_argument('query_img',
                               type=Path,
-                              help='Path to the root directory of the database')
-    parser_build.add_argument('source_imgs',
-                              type=Path,
-                              help='Root directory for image files to scan')
-    parser_build.set_defaults(func=build_cmd)
-    parser_prepare = subparsers.add_parser(
-        'prepare', help='Prepare for querying by generating a query descriptor file')
-    parser_prepare.add_argument('query_img', type=Path, help='Path to the query image to read')
-    parser_prepare.add_argument(
-        'query_des',
-        type=Path,
-        help=
-        f'The query descriptor filepath to write. If it does not end in {_DESCRIPTOR_FILE_SUFFIX}, it will be appended automatically'
-    )
-    parser_prepare.set_defaults(func=prepare_cmd)
-    parser_query = subparsers.add_parser('query', help='Query the descriptor database')
-    parser_query.add_argument('--db',
-                              required=True,
-                              type=Path,
-                              help='Path to the root directory of the database')
-    parser_query.add_argument('query_des',
-                              type=Path,
-                              help='Path to the query descriptor file to scan the database against')
-    parser_query.set_defaults(func=query_cmd)
+                              help='Path to the query image')
+    parser.add_argument('img_root', type=Path, help='Path to the directory containing images to search through')
     args = parser.parse_args()
 
-    args.func(args, parser.error)
+    print(f'{ANSI_CL}Initializing workers...', end='\r')
+    with multiprocessing.Pool(args.workers, init_worker, [args.query_img, args.resize]) as pool:
+        all_matches = list(
+            pool.imap_unordered(compute_ngood, args.img_root.rglob('*'), chunksize=args.chunksize))
+    all_matches.sort()
+    print(f'{ANSI_CL}Top {NTOP} matches:')
+    for ngood, scan_path in all_matches[-NTOP:]:
+        print(scan_path, f'({ngood})')
 
 
 if __name__ == '__main__':
