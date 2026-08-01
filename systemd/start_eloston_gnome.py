694e775ff2fdbcdafd5d9e00243c277bb6aae6f3
Eloston <eloston@programmer.net>
2026-08-01 05:20:16 +0000

    Update start-eloston-gnome

--- systemd/start_eloston_gnome.py
+++ systemd/start_eloston_gnome.py
@@ -14,14 +14,47 @@ class RequestHandler(BaseHTTPRequestHandler):
         html = '''
             <html>
                 <head>
-                    <title>Start Desktop</title>
+                    <title>Desktop Control</title>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <style>
+                        * {
+                            box-sizing: border-box;
+                            margin: 0;
+                            padding: 0;
+                        }
+                        body {
+                            display: flex;
+                            justify-content: center;
+                            align-items: center;
+                            min-height: 100vh;
+                        }
+                        .btn-group {
+                            display: flex;
+                            flex-direction: column;
+                            align-items: center;
+                            gap: 5vmin;
+                        }
+                        .btn-start {
+                            font-size: 6vmin;
+                            padding: 6vmin;
+                            width: 70vmin;
+                        }
+                        .btn-stop {
+                            font-size: 4vmin;
+                            padding: 4vmin;
+                            width: calc(70vmin * 2 / 3);
+                        }
+                    </style>
                 </head>
                 <body>
-                    <button onclick="sendRequest()">Start Desktop</button>
+                    <div class="btn-group">
+                        <button class="btn-start" onclick="sendRequest('/start')">Start Desktop</button>
+                        <button class="btn-stop" onclick="sendRequest('/stop')">Stop Desktop</button>
+                    </div>
                     <script>
-                        function sendRequest() {
+                        function sendRequest(path) {
                             var xhr = new XMLHttpRequest();
-                            xhr.open('POST', '/', true);
+                            xhr.open('POST', path, true);
                             xhr.send();
                         }
                     </script>
@@ -32,14 +65,24 @@ class RequestHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         try:
-            print('Running systemctl stop gdm3')
-            output = subprocess.run(["systemctl", "stop", "gdm3"], check=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            print('stdout:', output.stdout)
-            print('stderr:', output.stderr)
-            print('Running systemctl start eloston-gnome@tty1')
-            output = subprocess.run(["systemctl", "start", "eloston-gnome@tty1"], check=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            print('stdout:', output.stdout)
-            print('stderr:', output.stderr)
+            if self.path == '/start':
+                print('Running systemctl stop gdm3')
+                output = subprocess.run(["systemctl", "stop", "gdm3"], check=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                print('stdout:', output.stdout)
+                print('stderr:', output.stderr)
+                print('Running systemctl start eloston-gnome@tty1')
+                output = subprocess.run(["systemctl", "start", "eloston-gnome@tty1"], check=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                print('stdout:', output.stdout)
+                print('stderr:', output.stderr)
+            elif self.path == '/stop':
+                print('Running systemctl start eloston-gnome-logout@tty1')
+                output = subprocess.run(["systemctl", "start", "eloston-gnome-logout@tty1"], check=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                print('stdout:', output.stdout)
+                print('stderr:', output.stderr)
+            else:
+                self.send_response(404)
+                self.end_headers()
+                return
             self.send_response(200)
             self.end_headers()
         except Exception:
